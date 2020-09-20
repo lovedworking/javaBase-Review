3e1e7b065f37f859723865428277bf868727f1f0
tukk <tukk@yonyou.com>
2020-09-20 20:35:21 +0800

    在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。  *

---
+++ src/main/java/algorithm/arrays/FindRepeatNum.java
@@ -0,0 +1,16 @@
+package algorithm.arrays;
+
+/**
+ * https://www.nowcoder.com/practice/623a5ac0ea5b4e5f95552655361ae0a8?tpId=13&tqId=11203&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ * 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+ *
+ *
+ *
+ */
+public class FindRepeatNum {
+    public static void main(String[] args) {
+
+    }
+
+
+}
